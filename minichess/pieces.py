7a06898b376adbe8530aaed2c1abc58183317af0
Michael <hiebertmd@gmail.com>
2020-10-21 17:22:30 -0400

    Starting to work out state tree

--- minichess/pieces.py
+++ minichess/pieces.py
@@ -1,4 +1,6 @@
 from typing import List
+from minichess.resources import *
+import numpy as np
 import enum
 
 class PieceColor(enum.Enum):
@@ -41,9 +43,52 @@ class Piece:
         '''
         raise NotImplementedError # TODO
 
-# Specific Piece Definitions
+    def _onehot(self) -> list:
+        '''
+            Returns
+            -------
+            numpy array representing the one-hot encoding of this piece:
+                [1, 0, 0, 0, 0, 0] = Pawn
+                [0, 1, 0, 0, 0, 0] = Knight
+                [0, 0, 1, 0, 0, 0] = Bishop
+                [0, 0, 0, 1, 0, 0] = Rook
+                [0, 0, 0, 0, 1, 0] = Queen
+                [0, 0, 0, 0, 0, 1] = King
+        '''
+        raise NotImplementedError
 
-# each piece has movement_lock
+    def vector(self) -> np.array:
+        '''
+            Returns
+            -------
+            numpy array representing the vector encoding of this piece:
+                ID +
+                COLOR (0 for white, 1 for black) +
+                ONEHOT
+        '''
+        return np.array([self.id, self.color.value] + self._onehot())
+
+    @staticmethod
+    def from_vector(vector: np.array):
+        '''
+            Static method that intakes a vector and returns the piece corresponding
+        '''
+        _id = vector[0]
+        color = PieceColor(vector[1])
+        onehot = vector[2:]
+
+        pieces = [
+            Pawn,
+            Knight,
+            Bishop,
+            Rook,
+            Queen,
+            King
+        ]
+
+        return pieces[np.argmax(onehot)](_id, color, 0, 0) # TODO this loses info, just for viz purposes
+
+# Specific Piece Definitions
 
 class Pawn(Piece):
     def __init__(self, _id, color, points, max_move_range):
@@ -53,7 +98,10 @@ class Pawn(Piece):
         pass # TODO
 
     def __str__(self):
-        return '♙' if self.color == PieceColor.WHITE else '♟︎'
+        return WHITE_PAWN if self.color == PieceColor.WHITE else BLACK_PAWN
+
+    def _onehot(self):
+        return [1, 0, 0, 0, 0, 0]
 
 class Knight(Piece):
     def __init__(self, _id, color, points, max_move_range):
@@ -63,7 +111,10 @@ class Knight(Piece):
         pass # TODO
 
     def __str__(self):
-        return '♘' if self.color == PieceColor.WHITE else '♞'
+        return WHITE_KNIGHT if self.color == PieceColor.WHITE else BLACK_KNIGHT
+
+    def _onehot(self):
+        return [0, 1, 0, 0, 0, 0]
 
 class Bishop(Piece):
     def __init__(self, _id, color, points, max_move_range):
@@ -73,7 +124,10 @@ class Bishop(Piece):
         pass # TODO
 
     def __str__(self):
-        return '♗' if self.color == PieceColor.WHITE else '♝'
+        return WHITE_BISHOP if self.color == PieceColor.WHITE else BLACK_BISHOP
+
+    def _onehot(self):
+        return [0, 0, 1, 0, 0, 0]
 
 class Rook(Piece):
     def __init__(self, _id, color, points, max_move_range):
@@ -83,7 +137,10 @@ class Rook(Piece):
         pass # TODO
 
     def __str__(self):
-        return '♖' if self.color == PieceColor.WHITE else '♜'
+        return WHITE_ROOK if self.color == PieceColor.WHITE else BLACK_ROOK
+
+    def _onehot(self):
+        return [0, 0, 0, 1, 0, 0]
 
 class Queen(Piece):
     def __init__(self, _id, color, points, max_move_range):
@@ -93,7 +150,10 @@ class Queen(Piece):
         pass # TODO
 
     def __str__(self):
-        return '♕' if self.color == PieceColor.WHITE else '♛'
+        return WHITE_QUEEN if self.color == PieceColor.WHITE else BLACK_QUEEN
+
+    def _onehot(self):
+        return [0, 0, 0, 0, 1, 0]
 
 class King(Piece):
     def __init__(self, _id, color, points, max_move_range):
@@ -103,7 +163,10 @@ class King(Piece):
         pass # TODO
 
     def __str__(self):
-        return '♔' if self.color == PieceColor.WHITE else '♚'
+        return WHITE_KING if self.color == PieceColor.WHITE else BLACK_KING
+
+    def _onehot(self):
+        return [0, 0, 0, 0, 0, 1]
 
 # Piece Actions
 
