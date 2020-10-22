5a03488969faf8210d1041d74b79f6570eaddf17
Michael Hiebert <mhiebert@mit.edu>
2020-10-22 19:12:26 -0400

    Some TODOs for later

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from minichess.minichess import MiniChess
 from minichess.pieces import PieceColor
 
+# TODO check / checkmate / terminal states
+# TODO point values
+# TODO generating action options
+
 if __name__ == "__main__":
     mc = MiniChess.init_from_rules(None)
     mc.display_ascii()
