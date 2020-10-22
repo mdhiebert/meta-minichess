c7808f6c976e5e64376f9943a7307217d020424a
Rishi Shah <rshah36@mit.edu>
2020-10-22 01:35:17 -0400

    added variance method for rules
    made_from_vector method static in Rules class

--- train.py
+++ train.py
@@ -6,16 +6,16 @@ if __name__ == "__main__":
     meta_iterations = 10_000
     
     # OUTER LOOP
-
     for meta_iteration in range(meta_iterations):
         # sample across metaset (change rules)
-        # rules = MiniChessRuleset(...)
-
-        # rules -> hyperparams
-
+        # use rules_variance.py to generate new MiniChessRuleset(...)
+        
+        # execute Meta Model
+        #   input: rules
+        #   output: model hyperparams
         # meta learning step
 
-        # fixed = pretrained_model
+        # fixed = pretrained_model -> unclear on how this is generated
         # varied = pretrained_model (maybe with modifications!)
 
         # 1-shot learning step w hyperparams
