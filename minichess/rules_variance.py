a08fe0e21aff3643a1fd52208869d56d8648eab2
Rishi Shah <rishi@rishis-mbp.lan>
2020-10-25 15:52:48 -0400

    reformat

--- minichess/rules_variance.py
+++ minichess/rules_variance.py
@@ -1,11 +1,12 @@
-from minichess.rules import *
+from minichess.rules import MiniChessRuleset
 import scipy.stats
 
 # if null, no seed set
 RANDOM_SEED = null
 
-def new_rule_set(vary_values=False):
-    """ returns new MiniChessRuleset object
+def new_rule_set(vary_values=False) -> MiniChessRuleset:
+    '''
+    Returns new MiniChessRuleset object
     Will vary piece values only if option set
-    """
+    '''
     raise NotImplmentedError()
