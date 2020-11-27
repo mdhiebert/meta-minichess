Average model bug fix

learning/alpha_zero/distributed/pytorch/NNet.py
@@ -1,4 +1,4 @@
-from copy import copy
+import copy
 import os
 import sys
 import time
@@ -15,25 +15,18 @@ import torch.optim as optim
 
 from .MCGardnerNNet import MCGardnerNNet as mcnet
 
-args = dict({
-    'lr': 0.001,
-    'dropout': 0.3,
-    'epochs': 10,
-    'batch_size': 64,
-    'cuda': torch.cuda.is_available(),
-    'num_channels': 512,
-})
-
 LEN_ACTION_SPACE = 1225
 
 class NNetWrapper(NeuralNet):
-    def __init__(self, game):
+    def __init__(self, game, args):
         self.nnet = mcnet(game, args)
         self.game = game
         self.board_x, self.board_y = (5, 5)
         self.action_size = LEN_ACTION_SPACE
 
-        # if args['cuda']: # can't happen with multiprocessing
+        self.args = args.copy()
+
+        # if self.args['cuda']: # can't happen with multiprocessing
         #     self.nnet.cuda()
 
     def train(self, examples):
@@ -42,29 +35,29 @@ class NNetWrapper(NeuralNet):
         """
         optimizer = optim.Adam(self.nnet.parameters())
 
-        if args['cuda']: # this is not multiprocessed so we can use CUDA
+        if self.args['cuda']: # this is not multiprocessed so we can use CUDA
             self.nnet = self.nnet.cuda()
 
         losses = []
 
-        for epoch in range(args['epochs']):
+        for epoch in range(self.args['epochs']):
             print('EPOCH ::: ' + str(epoch + 1))
             self.nnet.train()
             pi_losses = AverageMeter()
             v_losses = AverageMeter()
 
-            batch_count = int(len(examples) / args['batch_size'])
+            batch_count = int(len(examples) / self.args['batch_size'])
 
             t = tqdm(range(batch_count), desc='Training Net')
             for _ in t:
-                sample_ids = np.random.randint(len(examples), size=args['batch_size'])
+                sample_ids = np.random.randint(len(examples), size=self.args['batch_size'])
                 boards, pis, vs = list(zip(*[examples[i] for i in sample_ids]))
                 boards = torch.FloatTensor(np.array(boards).astype(np.float64))
                 target_pis = torch.FloatTensor(np.array(pis))
                 target_vs = torch.FloatTensor(np.array(vs).astype(np.float64))
 
                 # predict
-                if args['cuda']: # this is not multiprocessed so we can use CUDA
+                if self.args['cuda']: # this is not multiprocessed so we can use CUDA
                     boards, target_pis, target_vs = boards.contiguous().cuda(), target_pis.contiguous().cuda(), target_vs.contiguous().cuda()
 
                 # compute output
@@ -100,7 +93,7 @@ class NNetWrapper(NeuralNet):
         board = board[np.newaxis, :, :]
         # preparing input
         board = torch.FloatTensor(board.astype(np.float64))
-        # if args['cuda']: board = board.contiguous().cuda() # does not work with multiprocessing
+        # if self.args['cuda']: board = board.contiguous().cuda() # does not work with multiprocessing
         board = board.view(1, self.board_x, self.board_y)
         self.nnet.eval()
         with torch.no_grad():
@@ -131,7 +124,7 @@ class NNetWrapper(NeuralNet):
         filepath = os.path.join(folder, filename)
         if not os.path.exists(filepath):
             raise ("No model in path {}".format(filepath))
-        map_location = None if args['cuda'] else 'cpu'
+        map_location = None if self.args['cuda'] else 'cpu'
         checkpoint = torch.load(filepath, map_location=map_location)
         self.nnet.load_state_dict(checkpoint['state_dict'])
 
@@ -158,9 +151,10 @@ class NNetWrapper(NeuralNet):
         avg_dict = {}
 
         for key in first_dict:
-            avg_dict[key] = average(np.array([d[key] for d in state_dicts]))
+            avg_dict[key] = torch.mean(torch.stack([d[key].float() for d in state_dicts]), 0)
 
         self.nnet.load_state_dict(avg_dict)
+
 
         return avg_dict
 
@@ -170,8 +164,8 @@ class NNetWrapper(NeuralNet):
         return self_dict
 
     def __deepcopy__(self, memo):
-        net = NNetWrapper(self.game)
-        net.nnet = copy.deepcopy(self.net, memo)
+        net = NNetWrapper(self.game, self.args.copy())
+        net.nnet = copy.deepcopy(self.nnet, memo)
         net.board_x = self.board_x
         net.board_y = self.board_y
         net.action_size = self.action_size

scripts/train.py
@@ -7,6 +7,8 @@ from games.rifle import RifleChessGame
 from games.dark import DarkChessGame
 from games.atomic import AtomicChessGame
 
+import torch
+
 import coloredlogs
 import logging
 
@@ -35,9 +37,23 @@ if __name__ == "__main__": # for multiprocessing
 
     parser.add_argument('--probs', action='store', nargs='+', type=float, default=None, help='The probabilities of the games to consider during training. The ith probability corresponds to the ith game provided. If no value is provided, this defaults to a uniform distribution across the provided games. (default: uniform dist)')
 
+    parser.add_argument('--learning_rate', action='store', type=float, default=0.001, help='The learning rate during training.')
+
+    parser.add_argument('--dropout', action='store', type=float, default=0.3, help='Dropout rate during training.')
+
+    parser.add_argument('--epochs', action='store', type=int, default=10, help='Number of epochs during training.')
+
+    parser.add_argument('--batch_size', action='store', type=int, default=64, help='Batch size during training.')
+
+    parser.add_argument('--num_channels', action='store', type=int, default=512, help='Number of channels to use in the model during training.')
+
     parser.add_argument('--task_batch_size', action='store', type=int, default=4, help='The number of tasks to sample in a given metalearning iteration. Not used if len(games) <= 1. (default: 4)')
 
     parser.add_argument('--eval_on_baselines', action='store_true', default=False, help='If passed in, we will evaluate our model against random and greedy players and plot the win rates.')
+    
+    parser.add_argument('--use_cuda', action='store_true', default=torch.cuda.is_available(), help='If passed, force the system to use CUDA. (default: whether or not CUDA is available)')
+
+    parser.add_argument('--dont_use_cuda', action='store_true', default=False, help='Force the system NOT to use CUDA, even if its available (default: False)')
 
     parser.add_argument('--debug', action='store_true', default=False)
 
@@ -52,6 +68,17 @@ if __name__ == "__main__": # for multiprocessing
     else:
         coloredlogs.install(level='INFO')
 
+    # cuda logic
+    use_cuda = False
+    if args.use_cuda:
+        use_cuda = True
+    if args.dont_use_cuda:
+        use_cuda = False
+
+    if use_cuda:
+        log.info('Using CUDA.')
+    else:
+        log.info('Not using CUDA.')
 
     # initialize args
 
@@ -68,7 +95,13 @@ if __name__ == "__main__": # for multiprocessing
         'taskBatchSize': args.task_batch_size,
 
         'numWorkers': args.workers,
-        'cuda': True,
+
+        'lr': args.learning_rate,
+        'dropout': args.dropout,
+        'epochs': args.epochs,
+        'batch_size': args.batch_size,
+        'cuda': use_cuda,
+        'num_channels': args.num_channels,
 
         'evalOnBaselines': args.eval_on_baselines,
 
@@ -114,7 +147,7 @@ if __name__ == "__main__": # for multiprocessing
     log.info('Loading %s...', 'Minichess Variants')
 
     log.info('Loading %s...', nn.__name__)
-    nnet = nn(games[0])
+    nnet = nn(games[0], train_args)
 
     if train_args['load_model']:
         log.info('Loading checkpoint "%s/%s"...', *train_args['load_folder_file'])