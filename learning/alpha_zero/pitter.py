93e30519886f9bb1c0635089330682571d842437
Rishi Shah <rshah36@mit.edu>
2020-11-28 17:57:56 -0500

    implemented meta_test.py

---
+++ learning/alpha_zero/pitter.py
@@ -0,0 +1,259 @@
+import logging
+import os
+import sys
+from collections import deque
+from pickle import Pickler, Unpickler
+from random import shuffle
+
+import matplotlib.pyplot as plt
+
+import numpy as np
+from tqdm import tqdm
+
+from learning.alpha_zero.joat_arena import JOATArena as Arena
+from learning.alpha_zero.mcts import MCTS
+
+log = logging.getLogger(__name__)
+
+class JOATPitter():
+    """
+    This class executes the self-play + JOAT adaptation + JOAT evaluation. It uses the functions defined
+    in Game and MetaMCNet. args are specified in main.py.
+
+    Parameters
+    ----------
+    games :: List[Game] : the list of games to evaluate with
+
+    joat :: NNet : the JOAT network
+
+    args :: dict : args
+    """
+
+    def __init__(self, games, joat, args):
+        self.games = games
+        self.joat = joat
+        self.adapt_joat = joat
+        self.args = args
+        self.mcts = None
+        # self.trainExamplesHistory = []  # history of examples from args.numItersForTrainExamplesHistory latest iterations
+
+    def executeEpisode(self, game):
+        """
+        This function executes one episode of self-play, starting with player 1.
+        As the game is played, each turn is added as a training example to
+        trainExamples. The game is played till the game ends. After the game
+        ends, the outcome of the game is used to assign values to each example
+        in trainExamples.
+        It uses a temp=1 if episodeStep < tempThreshold, and thereafter
+        uses temp=0.
+        Returns:
+            trainExamples: a list of examples of the form (canonicalBoard, currPlayer, pi,v)
+                           pi is the MCTS informed policy vector, v is +1 if
+                           the player eventually won the game, else -1.
+        """
+        trainExamples = []
+        board = game.getInitBoard()
+        self.curPlayer = 1
+        episodeStep = 0
+
+        moves = 0
+
+        while True:
+            episodeStep += 1
+            canonicalBoard = game.getCanonicalForm(board, self.curPlayer)
+            temp = int(episodeStep < self.args.tempThreshold)
+
+            pi = self.mcts.getActionProb(canonicalBoard, temp=temp)
+            sym = game.getSymmetries(canonicalBoard, pi)
+            for b, p in sym:
+                trainExamples.append([b, self.curPlayer, p, None])
+
+            action = np.random.choice(len(pi), p=pi)
+            board, self.curPlayer = game.getNextState(board, self.curPlayer, action)
+
+            r = game.getGameEnded(board, self.curPlayer)
+            
+            moves += 1
+
+            if moves >= self.args.maxMoves:
+                r = 1e-4
+
+            if r != 0:
+                return [(x[0], x[2], r * ((-1) ** (x[1] != self.curPlayer))) for x in trainExamples]
+
+    def adapt(self):
+        """
+        Performs numEps episodes of self-play, and retrains the JOAT NN with
+        examples in trainExamples (which has a maximum length of maxlenofQueue).
+        It then pits the new neural network against the original JOAT, as well
+        as random & greedy baseline players (if eval_on_baselines==True).
+        """
+
+        for game in self.games:
+            losses = []
+            joatwinrates = []
+            rwinrates = []
+            gwinrates = []
+
+            # bookkeeping
+            log.info(f'Self-playing game {type(game).__name__} ...')
+
+            # run self play on game variante
+            variationTrainExamples = deque([], maxlen=self.args.maxlenOfQueue)
+
+            for _ in tqdm(range(self.args.numEps), desc="Self Play"):
+                self.mcts = MCTS(game, self.joat, self.args)  # reset search tree
+                variationTrainExamples += self.executeEpisode(game)
+
+            # save the iteration examples to the history 
+            self.trainExamplesHistory.append(variationTrainExamples)
+
+            if len(self.trainExamplesHistory) > self.args.numItersForTrainExamplesHistory:
+                log.warning(
+                    f"Removing the oldest entry in trainExamples. len(trainExamplesHistory) = {len(self.trainExamplesHistory)}")
+                self.trainExamplesHistory.pop(0)
+            # backup history to a file
+            # NB! the examples were collected using the model from the previous iteration, so (i-1)  
+            self.saveTrainExamples(type(game).__name__)
+
+            # shuffle examples before training
+            trainExamples = []
+            for e in self.trainExamplesHistory:
+                trainExamples.extend(e)
+            shuffle(trainExamples)
+
+            # training new network, keeping a copy of the old one
+            self.joat.save_checkpoint(folder=self.args.checkpoint, filename='temp.pth.tar')
+            joatmcts = MCTS(game, self.joat, self.args)
+
+            pi_v_losses = self.joat.train(trainExamples)
+
+            for pi,v in pi_v_losses:
+                losses.append((pi, v, type(game).__name__))
+
+            self.plot_current_progress(losses)
+
+            adapt_joatmcts = MCTS(game, self.adapt_joat, self.args)
+
+            # ARENA
+
+            log.info('PITTING ADAPTED AGAINST ORIGINAL JOAT')
+            arena = Arena(lambda x: np.argmax(joatmcts.getActionProb(x, temp=0)),
+                        lambda x: np.argmax(adapt_joatmcts.getActionProb(x, temp=0)), [game])
+            pwins, nwins, draws = arena.playGames(self.args.arenaComparePerGame)
+
+            log.info('NEW/PREV WINS : %d / %d ; DRAWS : %d' % (nwins, pwins, draws))
+
+            if self.args.evalOnBaselines:
+                arena = Arena('random',
+                            lambda x: np.argmax(adapt_joatmcts.getActionProb(x, temp=0)), [game])
+                pwins, nwins, draws = arena.playGames(self.args.arenaComparePerGame)
+                rwinrates.append(float(nwins) / float(pwins + nwins + draws))
+                self.plot_win_rate(rwinrates, 'Random')
+
+                arena = Arena('greedy',
+                            lambda x: np.argmax(adapt_joatmcts.getActionProb(x, temp=0)), [game])
+                pwins, nwins, draws = arena.playGames(self.args.arenaComparePerGame)
+                gwinrates.append(float(nwins) / float(pwins + nwins + draws))
+                self.plot_win_rate(gwinrates, 'Greedy')
+
+
+    def getCheckpointFile(self, iteration):
+        return 'checkpoint_' + str(iteration) + '.pth.tar'
+
+    def saveTrainExamples(self, iteration):
+        folder = self.args.checkpoint
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+        filename = os.path.join(folder, self.getCheckpointFile(iteration) + ".examples")
+        with open(filename, "wb+") as f:
+            Pickler(f).dump(self.trainExamplesHistory)
+        f.closed
+
+    def loadTrainExamples(self):
+        modelFile = os.path.join(self.args.load_folder_file[0], self.args.load_folder_file[1])
+        examplesFile = modelFile + ".examples"
+        if not os.path.isfile(examplesFile):
+            log.warning(f'File "{examplesFile}" with trainExamples not found!')
+            r = input("Continue? [y|n]")
+            if r != "y":
+                sys.exit()
+        else:
+            log.info("File with trainExamples found. Loading it...")
+            with open(examplesFile, "rb") as f:
+                self.trainExamplesHistory = Unpickler(f).load()
+            log.info('Loading done!')
+
+            # examples based on the model were already collected (loaded)
+            self.skipFirstSelfPlay = True
+
+    def plot_current_progress(self, losses):
+        def uniqueish_color(n):
+            """https://stackoverflow.com/a/17241345"""
+            return plt.cm.gist_ncar(np.random.random(n))
+
+        c_dict = {
+            'GardnerMiniChessGame': (1, 0, 0),
+            'BabyChessGame': (1, 0.5, 0),
+            'MalletChessGame': (1, 1, 0),
+            'RifleChessGame': (0.5, 1, 0),
+            'AtomicChessGame': (0, 1, 0),
+            'DarkChessGame': (0, 1, 0.5),
+            'MonochromaticChessGame': (0, 1, 1),
+            'BichromaticChessGame': (0, 0.5, 1)
+        }
+
+        l_dict = {
+            'GardnerMiniChessGame': [],
+            'BabyChessGame': [],
+            'MalletChessGame': [],
+            'RifleChessGame': [],
+            'AtomicChessGame': [],
+            'DarkChessGame': [],
+            'MonochromaticChessGame': [],
+            'BichromaticChessGame': []
+        }
+
+        plt.cla()
+        plt.clf()
+
+        for x,loss in enumerate(losses):
+            pi,v,name = loss
+
+            l_dict[name].append((x,pi,v,c_dict[name]))
+
+        for name,game in l_dict.items():
+            if len(game) == 0: continue
+            xs,ys,_,cs = map(list, zip(*game))
+            plt.scatter(xs,ys,c=cs, label=name, s=10)
+
+        plt.xlabel('Epochs (~10^2 Games)')
+        plt.ylabel('Policy Loss')
+        plt.legend()
+        plt.savefig('results/policy_loss.png')
+
+        plt.cla()
+        plt.clf()
+
+        for name,game in l_dict.items():
+            if len(game) == 0: continue
+            xs,_,ys,cs = map(list, zip(*game))
+            plt.scatter(xs,ys,c=cs, label=name, s=10)
+
+        plt.xlabel('Epochs (~10^2 Games)')
+        plt.ylabel('Value Loss')
+        plt.legend()
+        plt.savefig('results/value_loss.png')
+
+    def plot_win_rate(self, win_rates, opponent):
+
+        plt.cla()
+        plt.clf()
+
+        plt.plot(win_rates, c='r')
+
+        plt.title('Win Rate vs {}'.format(opponent))
+        plt.xlabel('Iteration (~10^2 Games)')
+        plt.ylabel('Win Rate')
+        plt.legend()
+        plt.savefig(f'results/win_rates_{opponent}.png')
---
+++ scripts/meta_test.py
@@ -0,0 +1,168 @@
+import argparse
+
+from games.gardner import GardnerMiniChessGame
+from games.baby import BabyChessGame
+from games.mallet import MalletChessGame
+from games.rifle import RifleChessGame
+from games.dark import DarkChessGame
+from games.atomic import AtomicChessGame
+
+import torch
+
+import coloredlogs
+import logging
+
+if __name__ == "__main__": # for multiprocessing
+    logging.getLogger().setLevel(logging.INFO)
+
+    parser = argparse.ArgumentParser(description='Test a JOAT minichess model via n-shot adaptation.')
+
+    parser.add_argument('--loading_path', action='store', default=None, help='Path to the JOAT model weights.')
+
+    # parser.add_argument('--iterations', action='store', default='500', type=int, help='Number of full AlphaZero iterations to run for training (default: 500)')
+
+    parser.add_argument('--episodes', action='store', default='100', type=int, help='Number of episodes of self-play for adaptation og JOAT (default: 100)' )
+
+    # parser.add_argument('--mcts_sims', action='store', default='200', type=int, help='Number of MCTS simulations to perform per action.')
+
+    parser.add_argument('--arenapergame', action='store', default='10', type=int, help='The number of Arena Games to conduct per game variant. This number will be divided in half to give the model equal reps as both black and white. If this is 0, Arena will be skipped. (default: 10)')
+
+    parser.add_argument('--max_moves', action='store', default='75', type=int, help='The maximum number of moves permitted in a minichess game before declaring a draw (default: 75)')
+
+    # parser.add_argument('--win_threshold', action='store', default='0.6', type=float, help='The win threshold above which a new model must reach during arena-play to become the new best model (default: 0.6)')
+
+    parser.add_argument('--workers', action='store', default='1', type=int, help='The number of workers to use to process self- and arena-play. A value >1 will leverage multiprocessing. (default: 1)')
+
+    parser.add_argument('--games', dest='games', action='store', nargs='+', default='gardner', choices=['gardner', 'mallet', 'baby', 'rifle', 'dark', 'atomic'], type=str, help='The games to consider during testing. The adapted JOAT model will be assessed for each variant. (default: just gardner)')
+
+    # parser.add_argument('--probs', action='store', nargs='+', type=float, default=None, help='The probabilities of the games to consider during training. The ith probability corresponds to the ith game provided. If no value is provided, this defaults to a uniform distribution across the provided games. (default: uniform dist)')
+
+    parser.add_argument('--learning_rate', action='store', type=float, default=0.001, help='The learning rate during adaptation.')
+
+    parser.add_argument('--dropout', action='store', type=float, default=0.3, help='Dropout rate during adaptation.')
+
+    parser.add_argument('--epochs', action='store', type=int, default=10, help='Number of epochs during adaptation.')
+
+    parser.add_argument('--batch_size', action='store', type=int, default=64, help='Batch size during adaptation.')
+
+    parser.add_argument('--num_channels', action='store', type=int, default=512, help='Number of channels to use in the model during adaptation.') #defined by the input model?
+
+    # parser.add_argument('--task_batch_size', action='store', type=int, default=4, help='The number of tasks to sample in a given metalearning iteration. Not used if len(games) <= 1. (default: 4)')
+
+    parser.add_argument('--eval_on_baselines', action='store_true', default=False, help='If passed in, we will evaluate our model against random and greedy players and plot the win rates.')
+    
+    parser.add_argument('--use_cuda', action='store_true', default=torch.cuda.is_available(), help='If passed, force the system to use CUDA. (default: whether or not CUDA is available)')
+
+    parser.add_argument('--dont_use_cuda', action='store_true', default=False, help='Force the system NOT to use CUDA, even if its available (default: False)')
+
+    parser.add_argument('--debug', action='store_true', default=False)
+
+    args = parser.parse_args()
+
+    # logging
+
+    log = logging.getLogger(__name__)
+
+    if args.debug:
+        coloredlogs.install(level='DEBUG')
+    else:
+        coloredlogs.install(level='INFO')
+
+    # cuda logic
+    use_cuda = False
+    if args.use_cuda:
+        use_cuda = True
+    if args.dont_use_cuda:
+        use_cuda = False
+
+    if use_cuda:
+        log.info('Using CUDA.')
+    else:
+        log.info('Not using CUDA.')
+
+    # arena per game logic
+
+    if args.arenapergame < 1:
+        log.ingo('Must have atleast one arena iteration')
+        raise ValueError('Must have atleast one arena iteration')
+
+    # initialize args
+
+    train_args = dict({
+        'numEps': args.episodes,              # Number of complete self-play simulations to provide JOAT during adaptation.
+        'tempThreshold': 15,        # ?
+        'maxlenOfQueue': 200000,    # Max number of game examples provided for adaptation.
+        'arenaComparePerGame': args.arenapergame,         # Number of games to play during arena play to assess JOAT adaptation.
+        'cpuct': 1,
+        'maxMoves': args.max_moves, # Per game before draw.
+
+        'numWorkers': args.workers,
+
+        'lr': args.learning_rate, # For adaptation.
+        'dropout': args.dropout,
+        'epochs': args.epochs,
+        'batch_size': args.batch_size,
+        'cuda': use_cuda,
+        'num_channels': args.num_channels, ## necessary??
+
+        'evalOnBaselines': args.eval_on_baselines, # If True, will compare adapted JOAT to random and greedy players as well as original JOAT.
+
+        'checkpoint': './temp/',
+        'load_model': not args.loading_path is None,
+        'load_folder_file': ('/'.join(args.loading_path.split('/')[:-1]),args.loading_path.split('/')[-1]),
+        'numItersForTrainExamplesHistory': 20,
+    })
+
+    # set up games
+
+    game_to_class = {
+        'gardner': GardnerMiniChessGame,
+        'baby': BabyChessGame,
+        'mallet': MalletChessGame,
+        'rifle': RifleChessGame,
+        'atomic': AtomicChessGame,
+        'dark': DarkChessGame
+    }
+
+    games = [game_to_class[g] for g in args.games]
+
+
+    # handle imports
+    if args.workers > 1:
+        from learning.alpha_zero.distributed.joat_coach import JOATCoach
+        from learning.alpha_zero.distributed.pytorch.NNet import NNetWrapper as nn
+        from learning.alpha_zero.distributed.utils import *
+        from learning.alpha_zero.distributed.pitter import JOATPitter
+    else:
+        from learning.alpha_zero.joat_coach import JOATCoach
+        from learning.alpha_zero.pytorch.NNet import NNetWrapper as nn
+        from learning.alpha_zero.utils import *
+        from learning.alpha_zero.pitter import JOATPitter
+
+    log.info('Loading %s...', 'Minichess Variants')
+
+    log.info('Loading %s...', nn.__name__)
+    joat = nn(games[0], train_args)
+
+    if train_args['load_model']:
+        log.info('Loading checkpoint "%s/%s"...', *train_args['load_folder_file'])
+        joat.load_checkpoint(train_args['load_folder_file'][0], train_args['load_folder_file'][1])
+    else:
+        log.warning('No JOAT model found!')
+        raise ValueError('No JOAT model found; must be passed in to assess during testing')
+
+    log.info('Loading JOAT Pitter...')
+
+        
+    c = JOATPitter(games, joat, train_args)
+
+    if train_args['load_model']:
+        log.info("Loading 'trainExamples' from file...")
+        c.loadTrainExamples()
+
+    if len(games) > 1:
+        log.info('Starting the metalearning process 🎉')
+        c.metalearn()
+    else:
+        log.info('Starting the learning process 🎉')
+        c.learn()
